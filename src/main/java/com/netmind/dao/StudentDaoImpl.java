8e27d5b89afdaff914be80ecd160482a445bb376
Sergio Parra Sevillano <serparsev@gmail.com>
2021-03-12 15:04:29 +0100

    JSON Crud created

--- src/main/java/com/netmind/dao/StudentDaoImpl.java
+++ src/main/java/com/netmind/dao/StudentDaoImpl.java
@@ -1,56 +1,45 @@
 package com.netmind.dao;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Properties;
+import java.util.List;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
-import com.netmind.business.StudentBlImpl;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.netmind.common.model.LocalDateSerializer;
 import com.netmind.common.model.Student;
 import com.netmind.dao.contracts.StudentDao;
 
 public class StudentDaoImpl implements StudentDao {
 
-	static Properties prop = null;
-	static InputStream input = null;
-
-	private static ArrayList<Student> studentList = null;
+	private static List<Student> studentList = null;
 	static final Logger logger = Logger.getLogger(StudentDaoImpl.class);
-	static JSONArray studentJsonList = null;
 
 	static {
 		studentList = new ArrayList<Student>();
-		studentJsonList = new JSONArray();
-	}
-
-	static {
-		prop = new Properties();
-		try {
-			input = StudentBlImpl.class
-					.getResourceAsStream("/config.properties");
-			prop.load(input);
-		} catch (IOException e) {
-			logger.error(e.getMessage());
-			throw new ExceptionInInitializerError(e);
-		}
 	}
 
 	@Override
 	public boolean add(Student student) {
 		logger.info("add method called");
 		studentList.add(student);
-
 		return true;
 	}
 
 	@Override
-	public boolean addToTxtFile(Student student) throws IOException {
+	public boolean addStudentToFile(Student student) throws IOException {
 		logger.info("addStudentToFile method called");
 		try (FileWriter fileWriter = new FileWriter(
 				FileManagerDao.getFileName("txt"), true);
@@ -65,22 +54,100 @@ public class StudentDaoImpl implements StudentDao {
 		return true;
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean addToJsonFile(Student student) throws IOException {
+		List<Student> studentJsonList = getAllFromJson();
+		studentJsonList.add(student);
 
-		JSONObject studentJson = new JSONObject();
-		studentJson.put("student", student.toJsonFile());
+		return writeToJsonFile(studentJsonList);
+	}
 
-		studentJsonList.add(studentJson);
+	@Override
+	public List<Student> getAllFromJson() {
+		List<Student> studentJsonList = null;
+		Type typeStudentsList = new TypeToken<List<Student>>() {
+		}.getType();
 
-		try (FileWriter file = new FileWriter(
-				FileManagerDao.getFileName("json"))) {
-			file.write(studentJsonList.toString());
-			file.flush();
+		studentJsonList = new Gson().fromJson(getJsonFile(), typeStudentsList);
+
+		if (studentJsonList == null) {
+			studentJsonList = new ArrayList<Student>();
+		}
+
+		return studentJsonList;
+	}
+
+	public static String getJsonFile() {
+		StringBuilder serializedStudent = new StringBuilder();
+		try (BufferedReader buff = new BufferedReader(
+				new FileReader(FileManagerDao.getFileName("json")));) {
+			String line;
+			while ((line = buff.readLine()) != null)
+				serializedStudent.append(line.trim());
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return serializedStudent.toString();
+	}
+
+	@Override
+	public boolean updateJsonFile(UUID uuid, Student updatedStudent) {
+		List<Student> studentJsonList = getAllFromJson();
+		Student oldStudent = null;
+
+		oldStudent = studentJsonList.stream()
+				.filter(s -> s.getUUId().equals(uuid)).findFirst().orElse(null);
+
+		if (oldStudent != null) {
+			oldStudent.setIdStudent(updatedStudent.getIdStudent());
+			oldStudent.setName(updatedStudent.getName());
+			oldStudent.setSurname(updatedStudent.getSurname());
+			oldStudent.setAge(updatedStudent.getAge());
+			oldStudent.setDateOfBirth(updatedStudent.getDateOfBirth());
+
+			return writeToJsonFile(studentJsonList);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean removeFromJsonFile(UUID uuid) {
+		List<Student> studentJsonList = getAllFromJson();
+		Student removedStudent = null;
+
+		removedStudent = studentJsonList.stream()
+				.filter(s -> s.getUUId().equals(uuid)).findFirst().get();
+
+		if (removedStudent != null) {
+			studentJsonList.remove(removedStudent);
+			return writeToJsonFile(studentJsonList);
+		} else {
+			return false;
+		}
+	}
+
+	public boolean writeToJsonFile(List<Student> studentJsonList) {
+		try (Writer writer = new FileWriter(FileManagerDao.getFileName("json"),
+				false)) {
+
+			GsonBuilder gsonBuilder = new GsonBuilder();
+			gsonBuilder.registerTypeAdapter(LocalDate.class,
+					new LocalDateSerializer());
+
+			Gson gson = new GsonBuilder()
+					.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+					.setPrettyPrinting().create();
+
+			gson.toJson(studentJsonList.toArray(), writer);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return false;
+		return true;
 	}
 }
